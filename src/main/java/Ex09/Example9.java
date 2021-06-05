3b84f71ca2a1af9a305ae32b4a69fb3c1d5d39df
AgentAttorney <84649872+AgentAttorney@users.noreply.github.com>
2021-06-05 18:40:41 -0400

    Initial commit

---
+++ src/main/java/Ex09/Example9.java
@@ -0,0 +1,31 @@
+package Ex09;
+import java.util.Scanner;
+
+/*
+ *  UCF COP3330 Summer 2021 Assignment 1 Solution
+ *  Copyright 2021 first_name last_name
+ */
+
+public class Example9 {
+    public static void main(String[] args)
+    {
+        Scanner input = new Scanner(System.in);
+        int gal = 350, count = 0;
+
+        System.out.println("Enter the length and width of the room.");
+        int length = input.nextInt();
+        int width = input.nextInt();
+        
+        int sq_feet = length * width;
+
+        while(true)
+        {
+            count++;
+            if(sq_feet <= (count * gal)) {
+                System.out.print("You will need to purchase " + count + " gallons of paint to cover " +
+                         sq_feet + " square feet.");
+                break;
+            }
+        }
+    }
+}
